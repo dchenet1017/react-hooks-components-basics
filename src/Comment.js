d89e88d43dc1065c8fa290cfad7644de1dd822a3
Dimitri Chenet <dchenet1017@gmail.com>
2021-10-11 11:56:34 -0500

    commit tree e44e1f6043d7a34ddcf337c7f83c49641f667036

--- src/Comment.js
+++ src/Comment.js
@@ -3,5 +3,22 @@ import React from "react";
 function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
+\
+// funciton Comment(){
+    // return <div>Naturally, I agree with this article.</div>;
+//}
 
+// function Article(){
+  //return (
+  //<div>Dear Reader: Bjarne Stroustrup has teh perfect lecture oration.</div>
+//   );
+// }
+//function App(){
+  //return(
+//     <div>
+//     <Article/>
+//     <Comment/>
+//     </div>
+//   );
+// }
 export default Comment;
